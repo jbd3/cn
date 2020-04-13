6db4e0635989826da385138db439323efd087744
Jake Douglas <jbrandondouglas@gmail.com>
2020-04-13 14:35:31 -0400

    smaller boarder

--- pages/index.js
+++ pages/index.js
@@ -86,7 +86,7 @@ export default function Index() {
               display: flex;
               flex-direction: column;
               align-items: center;
-              border-bottom: solid 10px white;
+              border-bottom: solid 1px white;
             }
             .hero {
               width: 100%;
